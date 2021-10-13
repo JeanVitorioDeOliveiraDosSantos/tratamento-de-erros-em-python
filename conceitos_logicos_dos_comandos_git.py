9c9d6d82207980e615c825dbdd96a8e6061fd4f1
JeanVitorioDeOliveiraDosSantos <jeaninobran@gmail.com>
2021-10-13 15:40:38 -0300

    novas definições adicionadas ao documento

--- conceitos_logicos_dos_comandos_git.py
+++ conceitos_logicos_dos_comandos_git.py
@@ -13,5 +13,11 @@
  
 # git remote add origin " link da pag onde vc fez o novo repositorio a ser efetuado o comando push "
  
-# push : ele vai uppar as suas alterações para o server
-  
+# push : ele vai uppar as suas alterações para o server (" muitas vezes no primeiro push dentro de 
+# uma branch vc terá que executar o comando que o git te pedirá ")
+
+# git reflog: verá os hitorico de commits  
+
+# git hist: verá o hitorico de commits assim como o raflog
+
+# git reset --hard " id alcançavel pelos historicos de commits ": isso permitira que vc navegue entre versões antigas e niovas do seu cod 
