df7c682088eeb146b5ce9ec35eed447dff7a0875
JeanVitorioDeOliveiraDosSantos <jeaninobran@gmail.com>
2021-10-13 15:17:33 -0300

    Agora as difinições estão salvas

---
+++ tratamento_de_erros.py/definições.py
@@ -0,0 +1,8 @@
+# try: vai rodar o cod desde que não tenha erros
+
+# except: caso ocorra um erro no cod ele irá funcionar muito parecido com o if 
+# ou seja com varios execept para ver qual erro  se encaixa 
+
+# else terá a mesma função que tinha no if, ou seja caso o execpt não detectar erro o else será rodado
+
+### O finally será rodado todavez mas no final do c
