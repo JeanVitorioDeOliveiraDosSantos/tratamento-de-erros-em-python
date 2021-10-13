efe7fd40c9a143371601f12c70a5cfc1bbdaac9f
JeanVitorioDeOliveiraDosSantos <jeaninobran@gmail.com>
2021-10-13 15:32:01 -0300

    trbalhandono nos conceitos logicos de cada coisa

---
+++ Agora_vamos_guardar_os_comandos_git.py
@@ -0,0 +1,17 @@
+# git init : Ele inicia o arquivo git
+
+# git status: ele mostrará o status atual da pag
+
+# git add . : adiciona todas as pastas pedentes ou 
+# seja adiciona tudo o que estiver pendente
+
+# git commit -m " ele faz um novo commit  "
+
+# git config --global user-name " aqui coloque sei=u nome de usuario registrado no GitHub "
+
+# git config --global user " Aqui coloque seu e-mail cadastrado no GitHub " : Altera o e-mail onde estão suas alterações
+ 
+# git remote add origin " link da pag onde vc fez o novo repositorio a ser efetuado o comando push "
+ 
+# push : ele vai uppar as suas alterações para o server
+  
