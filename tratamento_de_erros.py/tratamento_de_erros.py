d8ca77e4b1f1b7243e8e9cc532602636ed4afa3d
JeanVitorioDeOliveiraDosSantos <jeaninobran@gmail.com>
2021-10-20 16:52:16 -0300

    exercicio finalizado

--- tratamento_de_erros.py/tratamento_de_erros.py
+++ tratamento_de_erros.py/tratamento_de_erros.py
@@ -51,21 +51,27 @@ finally:
 # Crie uma exeption que faça uma interrupção caso um obj seja com um valor inteiro superior a 100
 
 class Numero:
-    b = str()
-    c = str() 
+    b = None
+    c = None 
+    y = None
+    IOO= None
 
-    b = int(input(" Qual o numero a para ser multiplicado? :  "))
-    c = int(input(" Qual o numero b para ser multiplicado? :  "))
-    a = b ** c 
-    h = a
-    IOO = 100
     try:
-        if IOO >= h:
-            h  = str()
-    except ValueError as NumeroDeErro:
+        b = int(input(" Qual o numero a para ser multiplicado? :  "))
+        c = int(input(" Qual o numero b para ser multiplicado? :  "))
+        a = b * c 
+        h = a
+        IOO = 100
+        if (h > IOO):
+            f = dict()
+            print(f[1])
+    except KeyError as NumeroDeErro:
         print(" Numeros maiores que 100 não serão imprimidos ")
     else:
         print(" O resultado é :  --> ")
         print(f"numero é :  {h}")
+        print(y)
     finally:
         print(" Opercao finalizada ")
+
+    ##########################################################################
