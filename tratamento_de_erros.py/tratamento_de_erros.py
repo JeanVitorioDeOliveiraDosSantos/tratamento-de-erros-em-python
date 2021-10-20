bd96eb8685a034fa80a1a7965995d87aa5b8a901
JeanVitorioDeOliveiraDosSantos <jeaninobran@gmail.com>
2021-10-20 15:20:29 -0300

    novas adicoes ao conteudo

--- tratamento_de_erros.py/tratamento_de_erros.py
+++ tratamento_de_erros.py/tratamento_de_erros.py
@@ -17,3 +17,36 @@ finally:
     print(" Apesar do erro o cod está funconando normal ")
 print(f" a lista a :{a} ")
 ### O finally será rodado todavez independenete das outras estruturas (execpt, try) mas no final do cod. 
+
+
+# Faça um programa que solicite ao usuário 2 números inteiros. A seguir, calcule e mostre a divisão do primeiro pelo segundo. 
+# Obrigatório a inclusão do bloco try-except nas leituras (ValueError) e no cálculo da divisão (ZeroDivisionError). 
+# O programa deve ter também uma clásula "finalmente" com a mensagem "FIM !!". Atenção: O programa só continua se não houver erro.
+
+numero1 = 0
+numero2 = 0
+numero3 = str()
+numero4 = 0
+try:
+    numero1 = int(input(" numero 1 "))
+    numero2 = int(input(" numero 2 "))
+except ValueError as ZeroDivisionError:
+    if (numero1 != int()):
+        numero3 = "Segundo numero digitado não é um inteiro"
+
+    else:
+        numero3 = "Primeiro numero digitado não é um inteiro"
+    
+    print(f" {numero3} ")
+else:
+    numero_resposta = numero1 / numero2
+    print("claculo sendo feito")
+    print(f" o resultado é : {numero_resposta} ")
+
+finally:
+    print("FIM")
+
+# /////// fim 
+
+
+
