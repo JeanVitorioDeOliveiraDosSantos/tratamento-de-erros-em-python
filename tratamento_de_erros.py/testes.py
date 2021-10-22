a05995174a1b901d15fd302a0e7e53894cf081a2
JeanVitorioDeOliveiraDosSantos <jeaninobran@gmail.com>
2021-10-22 17:10:38 -0300

    Primeiro commit do dia 22/10/2021

---
+++ tratamento_de_erros.py/testes.py
@@ -0,0 +1,98 @@
+
+            
+
+def jason():
+    print("sla")
+    
+
+        
+
+g = jason()
+
+try:
+    class Frutas:
+        def __init__(self, frutas, carne):
+            self.frutas = frutas
+            self.carne = carne
+        
+            if (self.frutas == "frango"and "gado" and "porco"):
+                u = list()
+                print(u[1])
+                raise IndexError('Fruta não pode ser um animal ')
+                return IndexError
+
+            if (self.carne == "maçã" and "laranja" and "abacaxi"):
+                u = list()
+                print(u[1])
+                raise IndexError(' Animal não pode ser uma fruta ')
+
+
+
+    f = Frutas("laranja", "frango")
+
+except IndexError as erro: 
+    raise
+    print(erro)
+
+else:
+    g = f.carne
+    h = f.frutas
+    print(f" {g}  ,  {h} ")
+
+finally:
+    print("fim")
+
+
+
+
+
+
+def teste_num(n, N):
+    assert n > N, (n,' é maior que  ',N)
+
+print(teste_num(6, 5))
+
+import random
+
+
+def pulou_sem_responder(n, m):
+ 
+
+    try:
+        d = n / m
+
+    except ValueError as ZeroDivisionError:
+        print(" erro de  calculo ")
+            
+
+        pass 
+        
+    else:
+        numero_resposta = numero1 / numero2
+        print("claculo sendo feito")
+        print(f" O resultado é : {numero_resposta} ")
+
+    finally:
+        print("FIM")
+
+
+
+
+    
+
+
+class motion:
+    def __init__(self):
+        self.saldo_p = input("deseja ver seu saldo?(S/N)").upper()
+        if (self.saldo_p == "N"):
+            print("até a proxima")
+        if (self.saldo_p == "S"):
+            self.endress = input(" qual seu endereço de conta? ")
+            self.saldo_v = random.randint(0, 1000000)
+            print(f" seu saldo é de {self.saldo_v} ")
+
+person1 = motion()
+
+
+d = 9 / 0
+print(d)
--- tratamento_de_erros.py/tratamento_de_erros.py
+++ tratamento_de_erros.py/tratamento_de_erros.py
@@ -75,3 +75,4 @@ class Numero:
         print(" Opercao finalizada ")
 
 # /////// fim 
+
